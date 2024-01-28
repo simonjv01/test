b4d2de6d5fe002c6466d4826ef0477b659f0138d
Simon Vargas <sjvargas01@gmail.com>
2024-01-27 23:38:52 -0500

    first commit

---
+++ helloworld.cob
@@ -0,0 +1,16 @@
+       identification division.
+       program-id. "HELLOWORLD".
+       author. SIMON VARGAS
+      *This is a comment in COBOL
+      *Columns 8-11 A margin
+      *Columns 12-72 B margin
+       environment division.
+
+       data division.
+
+       procedure division.
+       0100-START-HERE.
+           display "Hello World!".
+       STOP RUN.
+       end program HELLOWORLD.
+        
